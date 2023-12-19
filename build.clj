e77f6f1166802c0e3b86c658e8ec6b1467f0496b
Jakub Dundalek <dundalek@gmail.com>
2023-12-19 19:08:04 +0100

    build parameter to select example

--- build.clj
+++ build.clj
@@ -4,48 +4,60 @@
    [babashka.process :refer [shell]])
   (:import [io.github.humbleui.jwm Platform]))
 
-(def class-dir "target/classes")
-(def uber-file "target/example.jar")
-(def executable-file "target/example")
+(def target-path "target")
+(def class-dir (str target-path "/classes"))
+
+(defn ->main [{:keys [main]}]
+  (or main "humble"))
+
+(defn ->uber-file [params]
+  (str target-path "/" (->main params) ".jar"))
+
+(defn ->executable-file [params]
+  (str target-path "/" (->main params)))
 
 (def include-resources
-  (str (condp = Platform/CURRENT
-         Platform/MACOS ".*\\.dylib"
-         Platform/WINDOWS ".*\\.dll"
-         Platform/X11 ".*\\.so")
-       "|.*jwm.version"
-       "|.*skija.version"
-       "|.*\\.ttf")) ; default humble theme bundles a ttf font
+  (str
+   ;; uber jar contains native libs for all platforms, let's pickup only those for target platform
+   ;; this can be further improved by also picking only those for target architecture like x86_64 and arm64
+   (condp = Platform/CURRENT
+     Platform/MACOS ".*\\.dylib"
+     Platform/WINDOWS ".*\\.dll"
+     Platform/X11 ".*\\.so")
+   "|.*jwm.version"
+   "|.*skija.version"
+   ;; default humble theme bundles a ttf font
+   "|.*\\.ttf"))
 
 (def graalvm-home (System/getenv "GRAALVM_HOME"))
 (def native-image-bin (str graalvm-home "/bin/native-image"))
 
 (def basis (b/create-basis {:project "deps.edn"}))
 
+;; == Build API ==
+
 (defn clean [_]
   (b/delete {:path "target"}))
 
-(defn uber [_]
-  (clean nil)
+(defn uber [params]
+  (clean params)
   (b/copy-dir {:src-dirs ["src" "resources"]
                :target-dir class-dir})
   (b/compile-clj {:basis basis
                   :src-dirs ["src"]
                   :class-dir class-dir})
   (b/uber {:class-dir class-dir
-           :uber-file uber-file
+           :uber-file (->uber-file params)
            :basis basis
-           :main
-           ; 'example.jwm
-           ; 'example.skija
-           'example.humble}))
+           :main (symbol (str "example." (->main params)))}))
 
-(defn native [_]
-  (uber nil)
+(defn native [params]
+  (uber params)
   (shell
    native-image-bin
    "--initialize-at-build-time"
    "-J-Dclojure.compiler.direct-linking=true"
+
    "--initialize-at-run-time=io.github.humbleui.jwm.impl.RefCounted$_FinalizerHolder"
    "--initialize-at-run-time=io.github.humbleui.jwm.impl.Managed"
 
@@ -57,24 +69,21 @@
 
    ; "--trace-object-instantiation=java.lang.ref.Cleaner"
    ; "--trace-class-initialization=io.github.humbleui.skija.shaper.Shaper"
-   ; "--trace-class-initialization=io.github.humbleui.skija.Font"
-   ; "--trace-class-initialization=io.github.humbleui.skija.Typeface"
 
-   ; "-H:ConfigurationFileDirectories=native-image-config"
    ; "-H:ConfigurationFileDirectories=traced-config"
    ; "-H:ReflectionConfigurationFiles=reflect-config.json"
-   "-H:+JNI"
    "-H:JNIConfigurationFiles=traced-config/jni-config.json"
+   "-H:+JNI"
    (str "-H:IncludeResources=" include-resources)
 
-   "--no-fallback"
+   ;; Some extra reporting for debugging
    "-H:+ReportExceptionStackTraces"
    "--report-unsupported-elements-at-runtime"
-
    "--native-image-info"
    "--verbose"
    "-Dskija.logLevel=DEBUG"
 
+   "--no-fallback"
    "-jar"
-   uber-file
-   executable-file))
+   (->uber-file params)
+   (->executable-file params)))
